e67e04810b2df13f4d69aa7342ff223c356f532f
Tianodont <104167316+Tianodont@users.noreply.github.com>
2023-12-03 18:38:19 +0300

    Add files via upload

---
+++ antizuzgina.py
@@ -0,0 +1,18 @@
+import requests
+import re
+
+t = open("0.html", "r", encoding="utf-8").read()
+
+def find_answer(id):
+    url = "https://math-ege.sdamgia.ru/problem?id="+str(id)
+    pattern = '<div class="answer" style="display:none"><span style="letter-spacing: 2px;">[^<>=:" ]{5}: [a-zA-Z0-9,]*</span></div>'
+    r = requests.get(url).text
+    res = re.findall(pattern, r)[0].replace('<div class="answer" style="display:none"><span style="letter-spacing: 2px;">', "")[6:].replace("</span></div>", "")
+    return res
+
+def get_ans(t)  :  
+    k = re.findall("comments[0-9]*", t)
+    for i in range(len(k)):
+        print(i+1, find_answer(k[i].replace("comments", "")))
+
+get_ans(t)
