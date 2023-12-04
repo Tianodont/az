cb979de13eff80ccb7f50fda52d9a1ff197d64cb
Tianodont <104167316+Tianodont@users.noreply.github.com>
2023-12-04 21:07:28 +0300

    Add files via upload

--- antizuzgina.py
+++ antizuzgina.py
@@ -1,13 +1,16 @@
 import requests
 import re
 
-def find_answer(id):
+def find_answer(n, id):
     url = "https://math-ege.sdamgia.ru/problem?id="+str(id)
     try:
-        pattern = '<div class="answer" style="display:none"><span style="letter-spacing: 2px;">[^<>=:" ]{5}: [a-zA-Z0-9,]*</span></div>'
+        pattern = '<div class="answer" style="display:none"><span style="letter-spacing: 2px;">[^<>=:" ]{5}: [a-zA-Z0-9,\-]*</span></div>'
         r = requests.get(url).text
         res = re.findall(pattern, r)[0].replace('<div class="answer" style="display:none"><span style="letter-spacing: 2px;">', "")[6:].replace("</span></div>", "")
-        return res
+        if n<10:
+            return res + " "*(12-len(res)) + url
+        else:
+            return res + " "*(11-len(res)) + url
     except Exception:
         return "Unknown"
 
@@ -15,6 +18,5 @@ def get_ans(t)  :
     retu = {}
     k = re.findall("comments[0-9]*", t)
     for i in range(len(k)):
-        retu[i+1] =  find_answer(k[i].replace("comments", ""))
+        retu[i+1] =  find_answer(i+1, k[i].replace("comments", ""))
     return retu
-
--- main.py
+++ main.py
@@ -16,7 +16,6 @@ import antizuzgina as az
 driver = webdriver.Chrome() 
 chrome_options = Options()
 driver.get("https://math-ege.sdamgia.ru/")
-
 try:
     element = WebDriverWait(driver, 100000).until(
         EC.presence_of_element_located((By.ID, "tophref"))
@@ -25,9 +24,5 @@ finally:
     n = driver.page_source
     ans = az.get_ans(n)
     for i in range(len(ans)):
-        print("№"+str(i+1)+" "+ans[i+1])
+        print("N"+str(i+1)+" "+ans[i+1])
 input()
-
-#armatricsits@gmail.com
-#?9+k}2kWFfh:#kK
-#56144055
