900c1265fafc35e19b34d9dfada1fa636f6145f1
Tianodont <104167316+Tianodont@users.noreply.github.com>
2023-12-04 11:40:24 +0300

    Add files via upload

--- antizuzgina.py
+++ antizuzgina.py
@@ -1,18 +1,20 @@
 import requests
 import re
 
-t = open("1.html", "r", encoding="utf-8").read()
-
 def find_answer(id):
     url = "https://math-ege.sdamgia.ru/problem?id="+str(id)
-    pattern = '<div class="answer" style="display:none"><span style="letter-spacing: 2px;">[^<>=:" ]{5}: [a-zA-Z0-9,]*</span></div>'
-    r = requests.get(url).text
-    res = re.findall(pattern, r)[0].replace('<div class="answer" style="display:none"><span style="letter-spacing: 2px;">', "")[6:].replace("</span></div>", "")
-    return res
+    try:
+        pattern = '<div class="answer" style="display:none"><span style="letter-spacing: 2px;">[^<>=:" ]{5}: [a-zA-Z0-9,]*</span></div>'
+        r = requests.get(url).text
+        res = re.findall(pattern, r)[0].replace('<div class="answer" style="display:none"><span style="letter-spacing: 2px;">', "")[6:].replace("</span></div>", "")
+        return res
+    except Exception:
+        return "Unknown"
 
 def get_ans(t)  :  
+    retu = {}
     k = re.findall("comments[0-9]*", t)
     for i in range(len(k)):
-        print(i+1, find_answer(k[i].replace("comments", "")))
+        retu[i+1] =  find_answer(k[i].replace("comments", ""))
+    return retu
 
-get_ans(t)
