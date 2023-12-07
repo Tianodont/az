6af6ee2ea096293230d4f65fb46159878b986e42
Tianodont <104167316+Tianodont@users.noreply.github.com>
2023-12-07 22:40:45 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -13,16 +13,23 @@ except Exception:
     from selenium.webdriver.support.ui import WebDriverWait
     from selenium.webdriver.support import expected_conditions as EC
 import antizuzgina as az
+
+
 driver = webdriver.Chrome() 
 chrome_options = Options()
 driver.get("https://math-ege.sdamgia.ru/")
+
+
 try:
     element = WebDriverWait(driver, 100000).until(
         EC.presence_of_element_located((By.ID, "tophref"))
     )
 finally:
     n = driver.page_source
+    #inps = driver.find_elements(By.CLASS_NAME, "test_inp")
     ans = az.get_ans(n)
     for i in range(len(ans)):
         print("N"+str(i+1)+" "+ans[i+1])
+        #inps[i].send_keys(ans[i+1].split()[0])
+        
 input()
